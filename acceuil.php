e33e94c9b0cd4f47ae68cbe53d5d8b9b866d2000
Marceauctr <34096806+Marceauctr@users.noreply.github.com>
2018-01-18 08:57:36 +0100

    Update acceuil.php

--- acceuil.php
+++ acceuil.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <link rel="stylesheet" type="text/css" href="fulbert.css">
-  <meta charset="utf-8">
-</head>
-<body>
-
-<ul>
-  <li><a class="active" href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
-</ul>
-
-</body>
+  <head>
+    <link rel="stylesheet" type="text/css" href="fulbert.css">
+    <meta charset="utf-8">
+  </head>
+  
+  <body>
+      <ul>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+  </body>
 </html>
